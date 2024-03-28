0f0e8ba796edfc3e96730feb70848abcb3426d38
hungrycatX <hungrycat.dev@gmail.com>
2024-03-28 14:17:44 +0900

    refactor : AJAX 요청 함수화 및 DOM API 사용 최소화
    * getData() 함수가 추가
    * getData() 함수를 사용하여 AJAX 요청 데이터를 가져오도록 수정
    * 뉴스 리스트 아이템 생성, 추가방식을 수정하여 HTML 구조를 파악하기 쉽도록 개선

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ const content = document.createElement("div"); // 동적 콘텐츠를 담을 div
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json"; // 뉴스 API 엔드포인트 URL
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json"; // 뉴스 콘텐츠 API 엔드포인트 URL
 
-ajax.open("GET", NEWS_URL, false); // 뉴스 API에 GET 요청을 보냄
-ajax.send(); // 요청 전송
+function getData(url) {
+  // AJAX 요청을 위한 데이터 가져오기 함수 정의
+  ajax.open("GET", url, false); // 뉴스 API에 GET 요청을 보냄
+  ajax.send(); // 요청 전송
 
-const newsFeed = JSON.parse(ajax.response); // 뉴스 피드 JSON 형식으로 파싱
+  return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL); // 뉴스 피드 JSON 형식으로 파싱
 const ul = document.createElement("ul"); // ul 엘리먼트 생성
 
 window.addEventListener("hashchange", function () {
   // 해시 변경 시 이벤트 리스너 추가
   const id = location.hash.substr(1); // 해시 값에서 id 추출
 
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false); // 해당 뉴스 콘텐츠 API에 GET 요청을 보냄
-  ajax.send(); // 요청 전송
-
-  const newsContent = JSON.parse(ajax.response); // 뉴스 콘텐츠 파싱
+  const newsContent = getData(CONTENT_URL.replace("@id", id)); // 뉴스 콘텐츠 파싱
   const title = document.createElement("h1"); // h1 엘리먼트 생성
 
   title.innerHTML = newsContent.title; // 타이틀 설정
@@ -28,14 +30,15 @@ window.addEventListener("hashchange", function () {
 
 for (let i = 0; i < 10; i++) {
   // 뉴스 피드를 반복하여 리스트 생성
-  const li = document.createElement("li"); // li 엘리먼트 생성
-  const a = document.createElement("a"); // a 엘리먼트 생성
+  const div = document.createElement("div");
 
-  a.href = `#${newsFeed[i].id}`; // a 태그의 href 속성 설정
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`; // a 태그의 텍스트 설정
-
-  li.appendChild(a); // li에 a를 자식으로 추가
-  ul.appendChild(li); // ul에 li를 자식으로 추가
+  // 뉴스 리스트를 생성하고 해당 뉴스로 이동하는 링크 추가
+  div.innerHTML = `
+  <li>
+    <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+  </li>
+  `;
+  ul.appendChild(div.firstElementChild); // ul에 div태그의 첫 번째 자식요소 li태그 추가
 }
 
 container.appendChild(ul); // 컨테이너에 ul 추가
